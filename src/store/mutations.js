41c868b7d78fb74068cf3f3a04d7a22084048f09
eugenewei <eugenewei1996@163.com>
2018-12-20 19:26:00 +0800

    feat: 添加addToCart方法

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -34,5 +34,24 @@ export default {
       }
       return item
     })
+  },
+  [mutationTypes.addToCart] (state, cartItem) {
+    const isInCart = state.cart.some(item => {
+      return item.id === cartItem.id
+    })
+    if (isInCart) {
+      state.cart = state.cart.map((item) => {
+        if (item.id === cartItem.id) {
+          item.count += 1
+        }
+        return item
+      })
+    } else {
+      state.cart.push({
+        ...cartItem,
+        count: 1,
+        isChecked: false
+      })
+    }
   }
 }
